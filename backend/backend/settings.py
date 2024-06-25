d720933f0064c5696bf5df3805e98fc36ea3878a
shironoir <vlaxeeh@gmail.com>
2024-06-25 16:33:48 +0300

    fixes in dependencies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*2w!h2ii&ou)dbmi*$kk!fvv^qu=-o!hvzwckxk7brf93=&ri5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
